0071bdfc6953ad4ea41818815554b40529926566
mohitrajane <mhit98@gmail.com>
2019-07-08 10:20:19 +0530

    save file as jpg

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from attendance_checker import attendance_check
 
 attendance, image = attendance_check(40716, 19971112)
-print(attendance)
-with open('test9595', 'wb') as out_file:
+img = attendance['admission_no'].split('/')
+print(img)
+img_file = img[0] + img[1] + '.jpg'
+with open(img_file, 'wb') as out_file:
     out_file.write(image)
